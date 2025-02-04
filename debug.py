3d7d3f6f44fe2f71d3bd5f6150b71a2d143a9c03
Ramdhan Hidayat <ramdhan.h@gmail.com>
2025-02-04 21:58:34 +0700

    feat: solution for debug question

--- debug.py
+++ debug.py
@@ -1,10 +1,26 @@
-def get_sum_metrics(predictions, metrics=[]):
-    for i in range(3):
-        metrics.append(lambda x: x + i)
+import pdb
 
-    sum_metrics = 0
-    for metric in metrics:
-        sum_metrics += metric(predictions)
+
+def get_sum_metrics(predictions, metrics=None):
+    """
+    Computes and sums up all the metric functions applied to the prediction.
+
+    Args:
+    predictions - A single numeric prediction value.
+    metrics - A list of functions that take a number and return a number.
+
+    Returns:
+    sum_metrics - The sum of all metric function outputs.
+    """
+    # pdb.set_trace()
+    if metrics is None:  # Fix mutable default argument issue
+        metrics = []
+
+    for i in range(3):
+        # pdb.set_trace()
+        metrics.append(lambda x, i=i: x + i)  # Capture i correctly
+
+    sum_metrics = sum(metric(predictions) for metric in metrics)
 
     return sum_metrics
 
@@ -13,10 +29,12 @@ def main():
     print(get_sum_metrics(0))  # Should be (0 + 0) + (0 + 1) + (0 + 2) = 3
     print(get_sum_metrics(1))  # Should be (1 + 0) + (1 + 1) + (1 + 2) = 6
     print(get_sum_metrics(2))  # Should be (2 + 0) + (2 + 1) + (2 + 2) = 9
-    print(get_sum_metrics(3, [lambda x: x]))  # Should be (3) + (3 + 0) + (3 + 1) + (3 + 2) = 15
+    # Should be (3) + (3 + 0) + (3 + 1) + (3 + 2) = 15
+    print(get_sum_metrics(3, [lambda x: x]))
     print(get_sum_metrics(0))  # Should be (0 + 0) + (0 + 1) + (0 + 2) = 3
     print(get_sum_metrics(1))  # Should be (1 + 0) + (1 + 1) + (1 + 2) = 6
     print(get_sum_metrics(2))  # Should be (2 + 0) + (2 + 1) + (2 + 2) = 9
+
 
 if __name__ == "__main__":
     main()
